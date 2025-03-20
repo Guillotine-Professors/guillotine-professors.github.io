be2be72f56c257d8aa86cbd26ba6e0e911dffd2e
Guillotine University Professor Union <guillotine.univ@proton.me>
2025-03-20 17:15:02 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,55 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/nextjs-github-pages/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import { useState } from "react";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/nextjs-github-pages/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/nextjs-github-pages/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/nextjs-github-pages/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/nextjs-github-pages/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+function MakeText(ntime: Date, dtype:number){
+    //ntime: nowtime
+    //dtype: display type (0: normal, 1: 12.3 based, 2: december based)
+    const thedate = new Date(2024,11,3,0,0,0,0);
+    if(dtype==0){
+        return `${ntime.getFullYear()}년 ${ntime.getMonth()}월 ${ntime.getDate()}일 ${ntime.getHours()}시 ${ntime.getMinutes()}분 ${ntime.getSeconds()}초`
+    }
+    if(dtype==1){
+        const diff = (ntime.getTime()-thedate.getTime())/1000;
+        const hour = (diff/3600).toFixed(0);
+        const minute = (diff/60%60).toFixed(0);
+        const second = (diff%60).toFixed(0);
+        return `2024년 12월 3일 ${hour}시 ${minute}분 ${second}초`
+    }
+    if(dtype==2){
+        const diff = (ntime.getTime()-thedate.getTime())/1000;
+        const date = (diff/3600/24+2).toFixed(0);
+        const hour = (diff/3600%24).toFixed(0);
+        const minute = (diff/60%60).toFixed(0);
+        const second = (diff%60).toFixed(0);
+        return `2024년 12월 ${date}일 ${hour}시 ${minute}분 ${second}초`
+    }
+    return "모르겠는"
+}
+
+export default function HomePage(){
+    // const date = new Date();
+    const [date, setDate] = useState(new Date);
+    const [dtype, setDtype] = useState(0);
+    setTimeout(()=>setDate(new Date()),1000)
+
+    return (<><Row style={{padding:"10%"}}>
+        <Row><h2>현재 시각은?</h2></Row>
+    <Row style={{display:'flex', textAlign:"center"}}>
+        <Col style={{width:"80%"}}>
+        <h3>
+        지금은 {MakeText(date, dtype)} 입니다.</h3>
+        </Col>
+    </Row>
+    <Row >
+        {dtype==0?<></>:<Button style={{marginTop:0.3+"em", marginBottom:0.5+"em"}}onClick={()=>setDtype(0)}>윤석열이 파면되었다면 지금은 몇시입니까?</Button>}
+        {dtype==1?<></>:<Button style={{marginTop:0.3+"em", marginBottom:0.5+"em"}}onClick={()=>setDtype(1)}>거짓말이야 나는 12월 3일을 벗어나지 못했어</Button>}
+        {dtype==2?<></>:<Button style={{marginTop:0.3+"em", marginBottom:0.5+"em"}}onClick={()=>setDtype(2)}>거짓말이야 나는 12월을 벗어나지 못했어</Button>}
+    </Row>
+    </Row>
+    </>);
 }
--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,8 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  // basePath: "/123time",
+  // basePath: "/",
 
   /**
    * Disable server-based image optimization. Next.js does not support
